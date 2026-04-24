8f09d8b1c1c3f7fb27df53f8af1fd72f537e53d8
dostayesky <156778419+dostayesky@users.noreply.github.com>
2026-04-24 21:28:38 +0700

    init

---
+++ variables.tf
@@ -0,0 +1,42 @@
+# ==========================================
+# 📁 ไฟล์: variables.tf
+# ==========================================
+
+variable "aws_region" {
+  description = "AWS Region ที่ต้องการ Deploy ระบบ"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "project_name" {
+  description = "ชื่อโปรเจกต์ (ใช้ตั้งชื่อ Resource ต่างๆ)"
+  type        = string
+  default     = "peaceful-workspace"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR Block สำหรับ VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "container_port" {
+  description = "Port ที่แอป Node.js ใช้งาน"
+  type        = number
+  default     = 3000
+}
+
+variable "app_image_url" {
+  description = "URL ของ Docker Image ใน ECR (รวม tag :latest)"
+  type        = string
+}
+
+variable "sagemaker_images" {
+  type = map(string)
+  default = {
+    "ap-southeast-1" = "763104351884.dkr.ecr.ap-southeast-1.amazonaws.com/huggingface-pytorch-inference:2.1.0-transformers4.37.0-cpu-py310-ubuntu22.04"
+    "us-east-1"      = "763104351884.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-inference:2.1.0-transformers4.32.2-cpu-py310-ubuntu22.04"
+    "ap-northeast-1" = "763104351884.dkr.ecr.ap-northeast-1.amazonaws.com/huggingface-pytorch-inference:2.1.0-transformers4.32.2-cpu-py310-ubuntu22.04"
+    # เพิ่ม Region อื่นๆ ได้ตามต้องการครับ
+  }
+}
